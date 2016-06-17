a18b4233f4fd0142904c8dbf2251d9dc71460d77
Yutaka Kamei <kamei@ykamei.net>
2016-06-17 22:36:23 +0900

    Update config.fish - set LANG

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,9 @@
 if status --is-login
-    set PATH $PATH ~/bin
+	set PATH $PATH ~/bin
+end
+if set --quqery LANG
+	set --global LANG en_US.UTF-8
 end
 set --global fish_prompt_pwd_dir_length 0
 set --global --export EDITOR vim
+
