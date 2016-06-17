8dea5997a2f7b2c366f65dccab553988068a9aa5
Yutaka Kamei <kamei@ykamei.net>
2016-06-17 22:32:31 +0900

    Update fish_prompt - We use fish_color_host as host specific color

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt
 		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
 	end
 
-	echo -s (set_color -b red) "$USER" @ "$__fish_prompt_hostname" \
+	echo -s (set_color -b $fish_color_host) "$USER" @ "$__fish_prompt_hostname" \
 		    (set_color normal) ' ' \
 		    (set_color $fish_color_cwd) (prompt_pwd)
 	echo -n -s (set_color normal) ' $ '
