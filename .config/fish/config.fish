a97add527a5f2980ba3f972fea65969b7dc2f093
Yutaka Kamei <kamei@ykamei.net>
2016-06-19 16:45:52 +0900

    Add ssh-agent settings in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,10 @@ if not set --query LANG
 end
 set --global fish_prompt_pwd_dir_length 0
 set --global --export EDITOR vim
+
+# SSH settings
+set -l agent_sock /tmp/.(whoami)-ssh
+if not test -S $agent_sock
+	ssh-agent -a $agent_sock 1> /dev/null
+end
+set --global --export SSH_AUTH_SOCK $agent_sock
